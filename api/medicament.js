8891fd53d284500ee5eba6d0d4b57f973bdce440
Ali Boulama <lg5uf7@gmail.com>
2026-02-19 09:15:44 +0000

    Refactor medicament.js to use ES6 module syntax

--- api/medicament.js
+++ api/medicament.js
@@ -1,10 +1,11 @@
-const axios = require('axios');
-const https = require('https');
+import axios from 'axios';
+import https from 'https';
 
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   const { nom, dci } = req.query;
 
@@ -46,4 +47,4 @@ module.exports = async function handler(req, res) {
       data: error.response?.data
     });
   }
-};
+}
